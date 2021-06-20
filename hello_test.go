88df0691ee9a70aa851a04ff9a7037997cf27df6
duy.nguyen <duy.nguyen@vnlp.ai>
2021-06-21 00:24:26 +0700

    finish hello, test

--- hello_test.go
+++ hello_test.go
@@ -3,10 +3,35 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("Duy")
-	want := "Hello, Duy"
 
-	if got != want {
-		t.Errorf("got %q want %q", got, want)
+	assertCorrectMessage := func(t testing.TB, got, want string) {
+		t.Helper()
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
 	}
+
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("Duy", "")
+		want := "Hello, Duy"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
+		got := Hello("", "")
+		want := "Hello, World"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in Spanish", func(t *testing.T) {
+		got := Hello("Duy", "Spanish")
+		want := "Hola, Duy"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in French", func(t *testing.T) {
+		got := Hello("Duy", "French")
+		want := "Bonjour, Duy"
+		assertCorrectMessage(t, got, want)
+	})
 }
